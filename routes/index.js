5c7493349a2c1f7c1c415177dc7ed8afb0c852e0
mig <michaelleojacob@gmail.com>
2022-09-10 21:05:51 -0700

    dates are formatted, message order is reversed, pages are styled

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,39 @@
 var express = require('express');
 var router = express.Router();
+const moment = require('moment');
 
 const messages = [
   {
     text: 'Hi there!',
     user: 'Amando',
-    added: new Date(),
+    added: moment(new Date()).subtract(5, 'h'),
   },
   {
     text: 'Hello World!',
     user: 'Charles',
-    added: new Date(),
+    added: moment(new Date()).subtract(17, 'm'),
   },
 ];
 
 /* GET home page. */
 router
   .get('/', function (req, res, next) {
-    res.render('index', { title: 'Mini Messageboard', messages });
+    const formatDates = messages
+      .map((message) => ({
+        ...message,
+        added: moment(message.added).fromNow(),
+      }))
+      .reverse();
+
+    res.render('index', { title: 'Mini Messageboard', messages: formatDates });
   })
   .get('/new', (req, res, next) => {
     res.render('form', { title: 'New Message' });
   })
   .post('/new', (req, res, next) => {
-    console.log('this fired');
+    const { user, text } = req.body;
+    const added = new Date();
+    messages.push({ text, user, added });
     res.redirect('/');
   });
 
